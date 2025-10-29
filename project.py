5e7f3828b7b0581e88b1688b18499d64849bd9ff
Steven Lanciotti <steven.lanciotti@berkeley.edu>
2025-10-29 14:33:16 -0400

    update

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from numpy.random import default_rng as rng
 import numpy as np
 import json, urllib.request
 import time
+import os
 
 st.title("W209 Final Project - Mid Term Presentation")
 st.header(
@@ -14,8 +15,15 @@ st.header(
 st.markdown("We will visualize the movement of dogs via adoption. Our datasets will be from Kaggle and from Shelter Animals Count. We will use a map of the United States to show (1) where dogs are being adopted from and adopted to at a state-to-state level, and (2) which states tend to have more dogs leave them via adoption vs. enter them via adoption.")
 
 
-k_dog_desc = pd.read_csv("Data/allDogDescriptions.csv") 
-k_dog_travel = pd.read_excel("Data/dogTravel.xlsx")
+DATA_PATH1 = os.path.join("Data", "allDogDescriptions.csv")
+DATA_PATH2 = os.path.join("Data", "dogTravel.xlsx")
+
+
+k_dog_desc = pd.read_csv(DATA_PATH1) 
+k_dog_travel = pd.read_excel(DATA_PATH2)
+
+st.dataframe(k_dog_travel)
+
 
 dog_travel_df = k_dog_travel.dropna(subset=['FoundState'])
 
